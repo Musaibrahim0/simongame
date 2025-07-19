2629a1dc62959341377375e37415594896a48419
Musa Ibrahim <164417343+Musaibrahim0@users.noreply.github.com>
2025-07-20 00:50:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
-var gamePattern = []
-function NewSequence (){
-  var randomNumber = Math.floor(Math.random() * 4)
- var randomChoseColor = buttonColors[randomNumber];
+var gamePattern = [];
+var userClickedPattern = [];
 
- gamePattern.push(randomChoseColor)
-
- $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
+
+function newSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber]; 
+
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100); 
+
+  playSound(randomChosenColor); // ✅ using function
+}
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  console.log("User clicked:", userChosenColor);
+
+  userClickedPattern.push(userChosenColor);
+  console.log("User Clicked Pattern:", userClickedPattern);
+
+  playSound(userChosenColor); // ✅ using function
+});
